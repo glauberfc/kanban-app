b3f3c96184488e94974fc904d7bfb0eea29ce364
Glauber Castro <gfc714@gmail.com>
2018-03-31 19:00:37 -0300

    Initiating delete notes feature

---
+++ app/components/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import uuid from 'uuid'
+import Notes from './Notes'
+
+export default class App extends React.Component {
+  state = {
+    notes: [
+      {
+        id: uuid.v4(),
+        task: 'Learn React'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Do laundry'
+      }
+    ]
+  } 
+
+  render() {
+    const { notes } = this.state
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} onDelete={this.deleteNote} />
+      </div>
+    )
+  }
+
+  addNote = () => {
+    this.setState({
+      notes: [
+        ...this.state.notes,
+        {
+          id: uuid.v4(),
+          task: 'New Task'
+        }
+      ]
+    })
+  }
+
+  deleteNote = (id, e) => {
+    e.stopPropagation()
+
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== id)
+    })
+  }
+} 

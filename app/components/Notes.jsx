220984472508a7231547c24879ddb054ef9ee762
glauberfc-IFCE <gfc714@gmail.com>
2018-04-03 16:28:00 -0300

    Extracting Rendering from Note

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -6,10 +6,10 @@ export default ({ notes, onDelete = () => {} }) => (
   <ul>
     {notes.map(({ id, task }) =>
       <li key={id}>
-        <Note
-          onDelete={onDelete.bind(null, id)}
-          task={task}
-        />
+        <Note>
+          <span>{task}</span>
+          <button onClick={onDelete.bind(null, id)}>x</button>
+        </Note>
       </li>
     )}
   </ul>
